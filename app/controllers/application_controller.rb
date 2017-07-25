a7a838f07f57c79c8907afa4271d583715a5d74e
chenxiaoqing <pincha120@gmail.com>
2017-07-25 12:09:34 +0800

    implement carts index function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,17 +10,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-    @current_cart ||= find_cart
+   @current_cart ||= find_cart
   end
 
   private
 
   def find_cart
-    cart = Cart.find_by(id: session[:cart_id])
-    if cart.blank?
-      cart = Cart.create
-    end
-    session[:cart_id] = cart.id
-    return cart
+   cart = Cart.find_by(id: session[:cart_id])
+   if cart.blank?
+     cart = Cart.create
+   end
+   session[:cart_id] = cart.id
+   return cart
   end
 end
